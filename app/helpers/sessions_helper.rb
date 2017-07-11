6cf3f152f6ed51f439fe937e0e0f032fc6266607
JrodManU <jrodmanu@yahoo.com>
2017-07-11 11:08:26 -0400

    can remember user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,10 +12,28 @@ module SessionsHelper
   def current_user
     if user_id = session[:user_id]
       @current_user ||= User.find_by(id: user_id)
+    elsif user_id = cookies[:user_id]
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(:remember, cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def remember(user)
+    user.new_remember_token
+    cookies.permenant.singed[:user_id] = id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget(user)
+    user.clear_remember_digest
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,30 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   has_secure_password
+
+  def authenticated?(attribute, token)
+    digest = self.send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+  end
+
+  def new_remember_token
+    remember_token = User.digest(User.new_token)
+    update_attributes(:remember_digest, remember_token)
+  end
+
+  def clear_remember_digest
+    update_attributes(:remember_digest, nil)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def User.digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                              BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
